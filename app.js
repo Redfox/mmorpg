08eab8c8135f00ebacc561d0762ca181cd3d2768
Vitor Pereira <vitor_pereira.2013@hotmail.com>
2016-12-03 01:30:31 -0200

    disconect n' move player

--- app.js
+++ app.js
@@ -10,15 +10,39 @@ app.use('/cliente', express.static(__dirname + '/cliente'));
 serv.listen(2000);
 console.log("server started");
 
+var socket_list = [];
+
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket){
-     console.log('socket connection');
+     socket.id = Math.random();
+     socket.x = 0;
+     socket.y = 0;
+     socket.number = "" + Math.floor(10 * Math.random());
+     socket_list[socket.id] = socket;
 
-     socket.on('happy', function(data){
-         console.log('happy ' + data.reason);
-     });
-
-     socket.emit('serverMsg', {
-         msg:'hello'
-     });
+    socket.on('disconnect', function(){
+        delete socket_list[socket.id];
+    });
 });
+
+setInterval(function(){
+    var pack = [];
+    for(var i in socket_list)
+    {
+        var socket = socket_list[i];
+        socket.x++;
+        socket.y++;
+        pack.push({
+            x:socket.x,
+            y:socket.y,
+            number:socket.number
+        });
+    }
+
+    for(var i in socket_list){
+        var socket = socket_list[i];
+        socket.emit('newPositions', pack);
+    }
+
+    
+}, 1000/25);
